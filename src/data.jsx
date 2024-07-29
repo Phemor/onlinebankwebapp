47cae4fead9d3e9eb9ef9f1357b4a216c15184ce
Senyo <senyonicho@gmail.com>
2024-07-29 14:12:27 +0000

    image slider

--- src/data.jsx
+++ src/data.jsx
@@ -1,8 +1,8 @@
 const imageSlide = [
-    { url: '../public/images/coins.jpg', title: 'Image slider 1' },
-    { url: '../public/images/guysmiling.jpg', title: 'Image slider 2' },
-    { url: '../public/images/payimage.jpg', title: 'Image slider 3' }
-  ];
-  
-  export default imageSlide;
-  
+  { url: '/images/coins.jpg', title: 'Image slider 1' },
+  { url: '/images/guysmiling.jpg', title: 'Image slider 2' },
+  { url: '/images/ladysmiling.webp', title: 'Image slider 3' },
+  { url: '/images/payimage.jpg', title: 'Image slider 4' }
+];
+
+export default imageSlide;
